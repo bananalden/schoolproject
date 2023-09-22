e3ac3c5c1a248f717c40ea189a4edc937eda9207
bananalden <128482606+bananalden@users.noreply.github.com>
2023-09-22 14:43:21 +0800

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>User creation:</h1>
+    <form action="registration.php" method="post">
+    <label>Name:</label><br>
+    <input type="text" name="fname"><br>
+    <label>Email:</label><br>
+    <input type="text" name="email"><br>
+    <label>Username:</label><br>
+    <input type="text" name="usn"><br>
+    <label>Password:</label><br>
+    <input type="password" name="pass"><br>
+    <button type="submit">SUBMIT</button>
+
+    </form>
+</body>
+</html>
+
+<?php ?>
