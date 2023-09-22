47345fa54f8abf2c3c9df7ed447af42f0487aade
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2023-09-22 17:50:29 +0800

    css

--- login.php
+++ login.php
@@ -3,15 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Project</title>
+    <link rel="stylesheet" href="css/main.css">
+
 </head>
 <body>
-    <form action="login.php" method="post">
-        <label>USERNAME:</label><br>
-        <input type="text" name="usn"><br>
-        <label>PASSWORD:</label><br>
-        <input type="password" name="pswd"><br>
-        <button type="submit" value="log in">LOG IN</button>
+    <form id="login" action="login.php" method="post">
+        <div class="username">
+            <label>USERNAME:</label><br>
+            <input type="text" name="usn"><br>
+        </div>
+
+        <div class="password">
+            <label>PASSWORD:</label><br>
+            <input type="password" name="pswd"><br>
+        </div>
+        <button class="btn" type="submit" value="log in">LOG IN</button>
     </form>
 </body>
 </html>
