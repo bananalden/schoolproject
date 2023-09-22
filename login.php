418fe9bbb6954918a2085b128cd073ea3c18151b
bananalden <128482606+bananalden@users.noreply.github.com>
2023-09-22 11:26:24 +0800

    files moved

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label>USERNAME:</label><br>
+        <input type="text" name="usn"><br>
+        <label>PASSWORD:</label><br>
+        <input type="password" name="pswd"><br>
+        <button type="submit" value="log in">LOG IN</button>
+    </form>
+</body>
+</html>
+
+<?php 
+
+include("database.php");
+
+
+
+?>
