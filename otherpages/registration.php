ee90552145b079a48ccfda049f68f68fef49b776
bananalden <128482606+bananalden@users.noreply.github.com>
2023-09-24 15:56:29 +0800

    typed in the files

--- otherpages/registration.php
+++ otherpages/registration.php
@@ -10,6 +10,7 @@ use function PHPSTORM_META\map;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/registration.css">
 </head>
 <body>
     <h1>User creation:</h1>
