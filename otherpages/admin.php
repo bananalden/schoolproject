925950d2073e1d2ac2e3b0438b0b0533047cfb7d
bananalden <128482606+bananalden@users.noreply.github.com>
2023-12-09 06:49:06 +0800

    made admin authentication a script

--- otherpages/admin.php
+++ otherpages/admin.php
@@ -1,22 +1,5 @@
 <?php 
-    session_start();
-    $userID = $_SESSION["userId"];
-    $adminuser = $_SESSION["admincheck"];
-    $fullname = $_SESSION["fullName"];
-    $loggedin = $_SESSION["isloggedin"];
-
-    
-
-      if (empty($loggedin) || empty($loggedin) || empty($loggedin) || empty($loggedin)){
-            session_destroy();
-            header("location:login.php");
-      }
-
-      if($adminuser != "admin"){
-            session_destroy();
-            header("location:login.php");
-      }
-
+require 'adminauthentication.php';
 ?>
 
 <html>
