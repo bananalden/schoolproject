73f722b4239604066edd1ee18594b801e206eb4c
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2023-12-11 13:25:36 +0800

    front-end

--- otherpages/login.php
+++ otherpages/login.php
@@ -8,27 +8,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Page</title>
     <link rel="stylesheet" href="../css/login.css">
 
 </head>
 <body>
     <div class="title">
-        <h1>Login User</h1>
+        <h1> Employee Login Form </h1>   
     </div>
-    <form action="backend/logcheck.php" method="post">
-        <div class="container-1">
-            <label class="uname" for="uname">Username:</label><br>
-            <input id="username" type="text" placeholder="Enter Your Username" name="uname">
-        </div>
+    <form>  
+    <div class="container">   
+            <div class="user-details">
+                <label>Username : </label>   
+                <input type="text" placeholder="Enter Username" name="username" required>
+            </div>
 
-        <div class="container-2">
-            <label class="password" for="psword">Password:</label><br>
-            <input id="psw" type="password" placeholder="Enter Your Password" name="pword">
-        </div>
+            <div class="user-details">
+                <label>Password : </label>   
+                <input type="password" placeholder="Enter Password" name="password" required>
+            </div>  
 
-        <button id="btn" type="submit" name="submit">login</button>
-    </form>
+            <button type="submit">Login</button>   
+            <input type="checkbox" checked="checked" class="remember"> Remember me   
+            <button type="button" class="cancelbtn"> Cancel</button>   
+            Forgot <a href="#"> password? </a>   
+        </div>   
+    </form>  
 </body>
 </html>
 
--- otherpages/registration.php
+++ otherpages/registration.php
@@ -7,7 +7,64 @@
     <link rel="stylesheet" href="../css/reg.css">
 </head>
 <body>
-    <h1>User creation:</h1>
+<div class="container">
+    <div class="title">Registration</div>
+    <div class="content">
+      <form action="#">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">Full Name</span>
+            <input type="text" placeholder="Enter your name" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Username</span>
+            <input type="text" placeholder="Enter your username" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="text" placeholder="Enter your email" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Phone Number</span>
+            <input type="text" placeholder="Enter your number" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="text" placeholder="Enter your password" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+        </div>
+        <div class="gender-details">
+          <input type="radio" name="gender" id="dot-1">
+          <input type="radio" name="gender" id="dot-2">
+          <input type="radio" name="gender" id="dot-3">
+          <span class="gender-title">Gender</span>
+          <div class="category">
+            <label for="dot-1">
+            <span class="dot one"></span>
+            <span class="gender">Male</span>
+          </label>
+          <label for="dot-2">
+            <span class="dot two"></span>
+            <span class="gender">Female</span>
+          </label>
+          <label for="dot-3">
+            <span class="dot three"></span>
+            <span class="gender">Prefer not to say</span>
+            </label>
+          </div>
+        </div>
+        <div class="button">
+          <input type="submit" value="Register">
+          <input type="submit" value="Back">
+        </div>
+      </form>
+    </div>
+  </div>
+    <!-- <h1>User creation:</h1>
     <form action="backend/regcheck.php" method="post">
         
         <div class="name":>
@@ -47,7 +104,7 @@
             <button class="back-btn" onclick="history.back()">Go Back</button>
         </div>
 
-    </form>
+    </form> -->
 
     <?php 
     
