c9b62bbf7ba173d927eef523956b843442b3714b
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2023-12-11 17:42:46 +0800

    front

--- otherpages/admin.php
+++ otherpages/admin.php
@@ -16,15 +16,31 @@ require 'backend/adminauthentication.php';
       <a class="create-button" href="registration.php">Create User</a>
       <a class="logout-button" href="logout.php">LOG OUT</a>
 </div>
+      <table class="content">
+            <tr class="header">
+                  <th>User ID</th>
+                  <th>Name</th>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Action</th>
+            </tr>
+      
+            <tr>
+                  <td>1</td>
+                  <td>Alden Flores</td>
+                  <td>aldenflores</td>
+                  <td>alden@gmail.com</td>
+                  <td><a>UPDATE</a><a>DELETE</a></td>
+            </tr>
+      </table>
 
-
-      <nav class="nav-bar">
+      <!-- <nav class="nav-bar">
             <ul>
                   <li>UserId</li>
                   <li>Name</li>
                   <li>>Username</li>
                   <li>Action</li>
             </ul>
-      </nav>
+      </nav> -->
 </body>
 </html>
