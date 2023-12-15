4fdbaa992191ccd68c42e8a25b87b89cc2d02832
bananalden <128482606+bananalden@users.noreply.github.com>
2023-12-15 14:53:13 +0800

    demo for timein

--- timeinfeaturetest/inserttable.php
+++ timeinfeaturetest/inserttable.php
@@ -1,10 +1,20 @@
 <?php 
+include 'database.php';
+$text = $_POST["value"];
+if(isset($_POST)){
+    
+    
+    
+    $sql = "INSERT INTO insertdata (poopoofart) VALUES (?);";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)){
 
-
-
-if(isset($_REQUEST["val"])){
-    $value = $_REQUEST["val"];
-
-    var_dump($value);
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "s", $text);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
 }
+
+
 ?>
