be91bdb67eb7d1496d8b3c12e978ef6a5b85390b
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-01-15 13:11:02 +0800

    updated 2

--- otherpages/admin.php
+++ otherpages/admin.php
@@ -10,20 +10,43 @@ require 'backend/database.php'
       
 </head>
 <body>
+
+      <nav> 
+            <div class="menu-icon">
+                  <span class="fas fa-bars"></span>
+            </div>
+
+            <div class="logo">Admin user</div>
+
+            <div class="nav-items">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Service</a></li>
+                  <li><a href="#">Gallery</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+            </div>
+
+            <div class="search-icon">
+                  <span class="fas fa-search"></span>
+            </div>
+
+            <div class="cancel-icon">
+                  <span class="fas fa-times"></span>
+            </div>
+
+            <form action="#">
+                  <input type="search" class="search-data" placeholder="Search" required>
+                  <button type="submit" class="fas fa-search"></button>
+            </form>
+
+      </nav> 
       
-            
-<nav class="topnav">
-            <ul>
-                  <li><a>Home</a></li>
-                  <li><a>Name</a></li>
-            </ul>
-</nav> 
-<div class="button-container"> 
-      <a class="create-button" href="registration.php">Create User</a>
-      <a class="logout-button" href="logout.php">LOG OUT</a>
-</div>
+      <div class="button-container"> 
+            <a class="create-button" href="registration.php">Create User</a>
+            <a class="logout-button" href="logout.php">LOG OUT</a>
+      </div>
       <table class="content">
-      <tr class="header">
+            <tr class="header">
                   <th>Employee ID</th>
                   <th>Full Name</th>
                   <th>Department</th>
@@ -31,7 +54,7 @@ require 'backend/database.php'
                   <th>Status</th>
                   <th>Action</th>
             </tr>
-      <?php include 'backend/usertable.php'; ?>
+            <?php include 'backend/usertable.php'; ?>
       </table>
 
   
