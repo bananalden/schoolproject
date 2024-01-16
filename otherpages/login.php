d4b5a196cb8538e1cb4aa273d057af67d52b0758
Mjmendiola <98451057+Mjmendiola@users.noreply.github.com>
2024-01-16 12:22:13 +0800

    NEW UPDATE 1

--- otherpages/login.php
+++ otherpages/login.php
@@ -48,7 +48,11 @@
             <a href="../index.php">Click here for the Time-In form for Employees!</a>
         </div>
        
-    </form>  
+    </form>
+    <div class="alert">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+        <strong>Wrong</strong>Credentials
+    </div>
 </body>
 </html>
 
